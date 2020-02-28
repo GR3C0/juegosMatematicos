4bf249dd5598df79f08b2505809658f408b2b902
Diego Morell <diegomorellmasip@gmail.com>
2020-02-28 01:19:16 +0100

    make login and styles

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, escape, request, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', db=read_db())
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', db=read_db())
+
+@app.route('/bomba<id>')
+def bomba(id):
+    template = "bomba{}.html".format(id)
+    return render_template(template)
+
+def read_db():
+    data = []
+    with open('grupos.json') as db:
+        data = json.load(db)
+        
+    return data
